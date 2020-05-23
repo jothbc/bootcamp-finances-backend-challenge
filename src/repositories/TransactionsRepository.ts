c23c0983fafcab7def06f6b1bdf7c7a4888c3a3b
jothbc <jothbc@gmail.com>
2020-05-23 16:51:21 -0300

    finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,14 @@
 import Transaction from '../models/Transaction';
 
-interface Balance {
+export interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+interface RequestDTO{
+  title:string;
+  value:number;
+  type: 'income'|'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +19,30 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let income=0,outcome=0,balance;
+    this.transactions.forEach(element => {
+      if(element.type=='income'){
+        income+=element.value;
+      }else{
+        outcome+=element.value;
+      }
+    });
+    balance = {
+      income,
+      outcome,
+      total: (income-outcome)
+    }
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title,value,type}:RequestDTO): Transaction {
+    const transaction = new Transaction({title,value,type});
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,5 +1,15 @@
-import TransactionsRepository from '../repositories/TransactionsRepository';
+import TransactionsRepository,{Balance} from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+
+interface Request{
+  title:string;
+  value:number;
+  type: 'income'|'outcome';
+}
+interface TransactionWithBalance{
+  transactions:Transaction[];
+  balance:Balance;
+}
 
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
@@ -8,8 +18,31 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title,value,type}:Request): Transaction {
+    if(type!='income' && type!='outcome'){
+      throw new Error('Type is incorrect');
+    }
+
+    if(type=='outcome'){
+      const balance = this.transactionsRepository.getBalance();
+      if(value>balance.total){
+        throw new Error ('Insufficient funds.');
+      }
+    }
+    const transaction = this.transactionsRepository.create({title,value,type});
+
+    return transaction;
+  }
+  public findAll():TransactionWithBalance{
+
+    const transactions = this.transactionsRepository.all();
+    const balance = this.transactionsRepository.getBalance();
+
+    const transactionWithBalance = {
+      transactions,
+      balance
+    }
+    return transactionWithBalance;
   }
 }
 
